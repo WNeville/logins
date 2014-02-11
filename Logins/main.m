18635e4c17ff6869cc3abb09d17504f426bc178e
William Neville <william@neville.com>
2014-02-10 21:28:07 -0500

    Initial Commit

---
+++ Logins/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Logins
+//
+//  Created by William Neville on 2/10/14.
+//  Copyright (c) 2014 William Neville. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
